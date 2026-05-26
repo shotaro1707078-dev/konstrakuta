f3d91ec6bdbd03c9efe9cb420bbcae78ddbe1dc3
4D-20\koyo <a>
2026-05-26 13:57:18 +0900

    能力値

--- konstrakuta/konstrakuta.cpp
+++ konstrakuta/konstrakuta.cpp
@@ -11,6 +11,12 @@ public:
 
 
 int HP;
+int MP;
+int ATK;
+int DEF;
+int SPD;
+
+
 
     //コンストラクタ
     Player()
@@ -20,22 +26,31 @@ int HP;
     }
 
 
-    Player(std::string name, int hp) 
-        :Name(name), HP(hp)
+    Player(std::string name, int hp=100,int mp=0,int atk=1,int def=1,int spd=1) 
+        :Name(name), HP(hp), MP(mp), ATK(atk), DEF(def), SPD(spd)
     {
 	}
 
 };
 
-
+void Show(const Player& player)
+{
+    std::cout << "Name:" << player.Name 
+        << ",HP:" << player.HP 
+		<< ",MP:" << player.MP
+		<< ",ATK:" << player.ATK
+		<< ",DEF:" << player.DEF
+		<< ",SPD:" << player.SPD
+        << std::endl;
+}
 
 int main()
 {
     Player player1;
-    Player playre2("勇者", 200);
+    Player playre2("勇者", 200,0,10,5,1);
 
 
-	std::cout << "Name:" << player1.Name << ",HP:" << player1.HP << std::endl;
-    std::cout << "Name:" << playre2.Name << ",HP:" << playre2.HP << std::endl;
+	Show(player1);
+    Show(playre2);
 }
 
